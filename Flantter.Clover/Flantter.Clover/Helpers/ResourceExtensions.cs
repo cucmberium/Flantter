2de532f5a1a15841f4587a815b43277322f44fc9
curi_u <cucmberium@users.noreply.github.com>
2019-02-10 22:07:32 +0900

    メインの画面を実装
    --
    先は長い

---
+++ Flantter.Clover/Flantter.Clover/Helpers/ResourceExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Windows.ApplicationModel.Resources;
+
+namespace Flantter.Clover.Helpers
+{
+    public static class ResourceExtensions
+    {
+        private static ResourceLoader _resourceLoader = new ResourceLoader();
+
+        public static string GetLocalized(this string resourceKey)
+        {
+            return _resourceLoader.GetString(resourceKey);
+        }
+    }
+}
---
+++ Flantter.Clover/Flantter.Clover/Views/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Core;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Flantter.Clover.ViewModels;
+using Prism.Windows.Mvvm;
+
+namespace Flantter.Clover.Views
+{
+    public sealed partial class MainPage : SessionStateAwarePage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // TitleBar
+            Window.Current.SetTitleBar(TitleBarMain);
+            var coreApplicationView = CoreApplication.GetCurrentView();
+            coreApplicationView.TitleBar.ExtendViewIntoTitleBar = true;
+            var applicationView = ApplicationView.GetForCurrentView();
+            applicationView.TitleBar.BackgroundColor =
+                ((SolidColorBrush)Application.Current.Resources["TitleBarBackgroundBrush"]).Color;
+            applicationView.TitleBar.ButtonBackgroundColor = Color.FromArgb(0x00, 0xff, 0xff, 0xff);
+            applicationView.TitleBar.ButtonForegroundColor =
+                ((SolidColorBrush)Application.Current.Resources["TitleBarButtonForegroundBrush"]).Color;
+            applicationView.TitleBar.ButtonInactiveBackgroundColor = Color.FromArgb(0x00, 0xff, 0xff, 0xff);
+            applicationView.TitleBar.ButtonInactiveForegroundColor =
+                ((SolidColorBrush)Application.Current.Resources["TitleBarButtonInactiveForegroundBrush"]).Color;
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            Frame.BackStack.Clear();
+            Frame.ForwardStack.Clear();
+        }
+        
+        public MainPageViewModel ViewModel
+        {
+            get => (MainPageViewModel)DataContext;
+            set => DataContext = value;
+        }
+    }
+}
